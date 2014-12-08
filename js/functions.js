92b9957a047f43c532f8b4469d4ee41bc70ee3d4
zolitariuz <raul@zamacona.me>
2014-12-08 14:42:08 -0600

    home

--- js/functions.js
+++ js/functions.js
@@ -2,11 +2,60 @@
 	"use strict";
 	$(function(){
 
+		/*------------------------------------*\
+			#ON LOAD
+		\*------------------------------------*/
+
 		/**
 		* Menu
 		**/
 		new mlPushMenu( document.getElementById( 'mp-menu' ), document.getElementById( 'trigger' ) );
 
-	});
 
+
+
+
+		/*------------------------------------*\
+			#Triggered events
+		\*------------------------------------*/
+
+
+
+
+		/*------------------------------------*\
+			#RESPONSIVE
+		\*------------------------------------*/
+
+	});
 })(jQuery);
+
+/*------------------------------------*\
+	#ON LOAD
+\*------------------------------------*/
+
+/**
+* Get the width of the window and apply it
+* as the height to the home secctions ( .square elements )
+**/
+
+
+function setSquareHeight(){
+	var windowWidth = $(window).width();
+	$('.j-square').height(windowWidth);
+}
+
+
+
+
+
+
+/*------------------------------------*\
+	#Triggered events
+\*------------------------------------*/
+
+
+
+
+/*------------------------------------*\
+	#RESPONSIVE
+\*------------------------------------*/
