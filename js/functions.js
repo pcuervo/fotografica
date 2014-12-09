318ac024a2d350b6f9332dadd251af98b874df80
zolitariuz <raul@zamacona.me>
2014-12-09 16:34:05 -0600

    quitar poner etc los filtros

--- js/functions.js
+++ js/functions.js
@@ -82,13 +82,66 @@ function showFilters(element){
 	$('.filters__content > div').css('height', '0px').removeClass('padding--small');
 	//Show this filter category
 	$('.filters__content .filter-'+filterCategory).addClass('padding--small').height('auto');
-
-
 }
 
+function addFilter(element){
+	//Clone this element so it won't get deleted by .append
+	// and manipulate ir instead of the clicked filter
+	var $clone = $(element).clone();
 
+	//If element is already added, then delete it
+	if ( $(element).hasClass('filter--active') ){
+		//Remove class active
+		$(element).removeClass('filter--active');
+
+		//Get its content so we can delete in the selected filters (.filters__results )
+		//If element has extra info, delete that info first
+		if ( $clone.hasClass('filter--info') ){
+			$clone.find('span').remove();
+		}
+		var filterContent = $clone.html();
+		$('.filters__results .filter:contains('+filterContent+')').remove();
+		return;
+	}
+
+	//Add active class to this element and its clone
+	$(element).addClass('filter--active');
+	$clone.addClass('filter--active');
+
+	//If element has extra info, delete that info
+	if ( $clone.hasClass('filter--info') ){
+		//First delete the info class
+		$clone.removeClass('filter--info')
+		//then delete the info
+		$clone.find('span').remove();
+	}
+
+	//And add this filter to the set of selected filters (.filters__results )
+	$clone.appendTo('.filters__results');
+}
+
+function removeFilter(element){
+	//Get its content so we can deactivate it
+	// in .filters__content
+	var filterContent = $(element).html();
+	console.log(filterContent);
+
+	//Delete it
+	$(element).remove();
+
+	//Deactive it in .filters__content
+	$('.filters__content .filter:contains('+filterContent+')').removeClass('filter--active');
+}
 
 
 /*------------------------------------*\
 	#RESPONSIVE
 \*------------------------------------*/
+
+
+
+
+
+
+
+
