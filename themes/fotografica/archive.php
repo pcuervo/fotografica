747ef39858e6cc3637ea899ea6c7af6ce071d1c5
zolitariuz <raul@zamacona.me>
2015-02-07 16:59:55 -0600

    más filtros en archive.php

--- themes/fotografica/archive.php
+++ themes/fotografica/archive.php
@@ -136,92 +136,113 @@
 		</div>
 	</section>
 
+	<!-- /** -->
+	 <!-- * If the post type is "proyectos" or "exposiciones" the do not display filters at all -->
+	<!-- **/ -->
+	<?php if ( $postType !== 'proyectos' AND $postType !== 'exposiciones' ){ ?>
 
-	<section class="[ filters ] [ margin-bottom--small ]">
-		<div class="[ filters__tabs ] [ clearfix ]">
-			<div class="[ wrapper ]">
-				<div class="[ row ]">
-					<!--  /********************************\ -->
-						<!-- #FOTOGRAFOS -->
-					<!--  \**********************************/ -->
-					<?php if ( $postType == 'fotografos' ){ ?>
-						<a class="[ tab-filter ] [ text-center ] [ columna xmall-4 medium-2 ]" href="#" data-filter="colecciones">Colecciones</a>
-						<a class="[ tab-filter ] [ text-center ] [ columna xmall-4 medium-2 ]" href="#" data-filter="pais">País</a>
-						<a class="[ tab-filter ] [ text-center ] [ columna xmall-4 medium-2 ]" href="#" data-filter="decada">Década</a>
-						<a class="[ tab-filter ] [ text-center ] [ columna xmall-4 medium-2 ]" href="#" data-filter="tema">Tema</a>
-						<a class="[ tab-filter ] [ text-center ] [ columna xmall-4 medium-2 ]" href="#" data-filter="apellido">Apellido</a>
-					<?php } ?>
-				</div><!-- row -->
-			</div><!-- wrapper -->
-		</div><!-- filters__tabs -->
-		<div class="[ filters__content ] [ text-center ]">
-			<!--  /********************************\ -->
-				<!-- #FOTOGRAFOS -->
-			<!--  \**********************************/ -->
-			<?php if ( $postType == 'fotografos' ){ ?>
-				<div class="[ filter-colecciones ]">
-					<?php
-						$args = array(
-							'orderby'		=> 'name',
-							'order'         => 'ASC',
-							'hide_empty'    => true
-						);
-						$terms = get_terms('coleccion', $args);
-						foreach ($terms as $key => $term) {
-					?>
-							<a class="[ filter filter--info ] [ button button--hollow button--small button--dark ] [ inline-block margin-bottom--small ]" data-type="coleccion" data-value="<?php echo $term->slug ?>"><?php echo $term->name ?><span><i class="fa fa-info-circle"></i></span></a>
-					<?php
-						}
-					?>
-				</div><!-- .filter-colecciones -->
-			<?php } ?>
-			<div class="[ filter-pais ]">
-			</div><!-- .filter-pais -->
-			<div class="[ filter-decada ]">
-				<?php
-					$args = array(
-					    'orderby'		=> 'name',
-					    'order'         => 'ASC',
-					    'hide_empty'    => true,
-					);
-					$terms = get_terms('año', $args);
-					$decadas = array();
-					foreach ($terms as $key => $term) {
-						if (strpos($term->name,'/') !== false) {
-							$decada = 'sin fecha';
-						} else if (strpos($term->name,'-') !== false) {
-						    $rango_fechas = explode('-', $term->name);
-						    $ano_inicial = substr(trim($rango_fechas[0]), 0, -1);
-						    $ano_inicial = $ano_inicial.'0';
-						    $ano_final = substr(trim($rango_fechas[1]), 0, -1);
-						    $ano_final = $ano_final.'0';
-						    array_push($decadas, $ano_inicial);
-						    array_push($decadas, $ano_final);
-						} else {
-							$decada = substr($term->name, 0, -1);
-							$decada = $decada.'0';
-							array_push($decadas, $decada);
-						}
-					}
-					$decadas = array_unique($decadas);
-					foreach ($decadas as $decada) {
-				?>
-						<a class="[ filter ] [ button button--hollow button--small button--dark ] [ inline-block margin-bottom--small ]" data-type="año" data-value="<?php echo $decada ?>"><?php echo $decada ?></a>
-				<?php
-					}
-				?>
-			</div><!-- .filter-decada -->
-			<div class="[ filter-tema ]">
-				<a class="[ filter filter--active ] [ button button--hollow button--small button--dark ] [ inline-block margin-bottom--small ]">#méxico</a>
-			</div><!-- .filter-tema -->
-			<div class="[ filter-apellido ]">
-				<!-- <a class="[ filter ] [ button button--hollow button--small button--dark ] [ inline-block margin-bottom--small ]">Z</a> -->
-			</div><!-- .filter-apellido -->
-		</div><!-- filters__content -->
-		<div class="[ filters__results ] [ padding--small text-center ]">
-			<p class="[ uppercase ] [ js-num-resultados ]"><span></span> resultados con los filtros:</p>
-		</div>
-	</section><!-- .filters -->
+		<section class="[ filters ] [ margin-bottom--small ]">
+			<div class="[ filters__tabs ] [ clearfix ]">
+				<div class="[ wrapper ]">
+					<div class="[ row ]">
+						<!--  /********************************\ -->
+							<!-- #FOTOGRAFOS -->
+						<!--  \**********************************/ -->
+						<?php if ( $postType == 'fotografos' ){ ?>
+							<a class="[ tab-filter ] [ text-center ] [ columna xmall-4 medium-2 ]" href="#" data-filter="colecciones">Colecciones</a>
+							<a class="[ tab-filter ] [ text-center ] [ columna xmall-4 medium-2 ]" href="#" data-filter="pais">País</a>
+							<a class="[ tab-filter ] [ text-center ] [ columna xmall-4 medium-2 ]" href="#" data-filter="decada">Década</a>
+							<a class="[ tab-filter ] [ text-center ] [ columna xmall-4 medium-2 ]" href="#" data-filter="tema">Tema</a>
+							<a class="[ tab-filter ] [ text-center ] [ columna xmall-4 medium-2 ]" href="#" data-filter="apellido">Apellido</a>
+						<?php } ?>
+						<!--  /********************************\ -->
+							<!-- #FOTOGRAFOS -->
+						<!--  \**********************************/ -->
+						<?php if ( $postType == 'evento' OR $postType == 'cartelera' ){ ?>
+							<a class="[ tab-filter ] [ text-center ] [ columna xmall-4 medium-2 ]" href="#" data-filter="fecha">Fecha</a>
+						<?php } ?>
+					</div><!-- row -->
+				</div><!-- wrapper -->
+			</div><!-- filters__tabs -->
+			<div class="[ filters__content ] [ text-center ]">
+				<!--  /********************************\ -->
+					<!-- #EVENTOS / CARTELERA -->
+				<!--  \**********************************/ -->
+				<?php if ( $postType == 'fotografos' ){ ?>
+					<div class="[ filter-colecciones ]">
+						<?php
+							$args = array(
+								'orderby'		=> 'name',
+								'order'         => 'ASC',
+								'hide_empty'    => true
+							);
+							$terms = get_terms('coleccion', $args);
+							foreach ($terms as $key => $term) {
+						?>
+								<a class="[ filter filter--info ] [ button button--hollow button--small button--dark ] [ inline-block margin-bottom--small ]" data-type="coleccion" data-value="<?php echo $term->slug ?>"><?php echo $term->name ?><span><i class="fa fa-info-circle"></i></span></a>
+						<?php
+							}
+						?>
+					</div><!-- .filter-colecciones -->
+					<div class="[ filter-pais ]">
+					</div><!-- .filter-pais -->
+					<div class="[ filter-decada ]">
+						<?php
+							$args = array(
+								'orderby'		=> 'name',
+								'order'         => 'ASC',
+								'hide_empty'    => true,
+							);
+							$terms = get_terms('año', $args);
+							$decadas = array();
+							foreach ($terms as $key => $term) {
+								if (strpos($term->name,'/') !== false) {
+									$decada = 'sin fecha';
+								} else if (strpos($term->name,'-') !== false) {
+									$rango_fechas = explode('-', $term->name);
+									$ano_inicial = substr(trim($rango_fechas[0]), 0, -1);
+									$ano_inicial = $ano_inicial.'0';
+									$ano_final = substr(trim($rango_fechas[1]), 0, -1);
+									$ano_final = $ano_final.'0';
+									array_push($decadas, $ano_inicial);
+									array_push($decadas, $ano_final);
+								} else {
+									$decada = substr($term->name, 0, -1);
+									$decada = $decada.'0';
+									array_push($decadas, $decada);
+								}
+							}
+							$decadas = array_unique($decadas);
+							foreach ($decadas as $decada) {
+						?>
+								<a class="[ filter ] [ button button--hollow button--small button--dark ] [ inline-block margin-bottom--small ]" data-type="año" data-value="<?php echo $decada ?>"><?php echo $decada ?></a>
+						<?php
+							}
+						?>
+					</div><!-- .filter-decada -->
+					<div class="[ filter-tema ]">
+						<a class="[ filter filter--active ] [ button button--hollow button--small button--dark ] [ inline-block margin-bottom--small ]">#méxico</a>
+					</div><!-- .filter-tema -->
+					<div class="[ filter-apellido ]">
+						<!-- <a class="[ filter ] [ button button--hollow button--small button--dark ] [ inline-block margin-bottom--small ]">Z</a> -->
+					</div><!-- .filter-apellido -->
+				<?php } ?>
+				<!--  /********************************\ -->
+					<!-- #EVENTOS / CARTELERA -->
+				<!--  \**********************************/ -->
+				<?php if ( $postType == 'evento' OR $postType == 'cartelera' ){ ?>
+					<div class="[ filter-fecha ]">
+						<a class="[ filter ] [ button button--hollow button--small button--dark ] [ inline-block margin-bottom--small ]">Próximos eventos</a>
+						<a class="[ filter ] [ button button--hollow button--small button--dark ] [ inline-block margin-bottom--small ]">Hoy</a>
+						<a class="[ filter ] [ button button--hollow button--small button--dark ] [ inline-block margin-bottom--small ]">Eventos anteriores</a>
+					</div><!-- .filter-fecha -->
+				<?php } ?>
+			</div><!-- filters__content -->
+			<div class="[ filters__results ] [ padding--small text-center ]">
+				<p class="[ uppercase ] [ js-num-resultados ]"><span></span> resultados con los filtros:</p>
+			</div>
+		</section><!-- .filters -->
+	<?php } ?>
 	<div class="[ loader ] [ center ]">
 		<div></div>
 	</div>
