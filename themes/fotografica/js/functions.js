1b5d44ceb522df11eaa4aaa5dd7aed1937037141
maichomper <maichomper@gmail.com>
2015-05-07 19:28:20 -0500

    poner proyecto, publicacion o exposicions random en te puede interesar

--- themes/fotografica/js/functions.js
+++ themes/fotografica/js/functions.js
@@ -392,7 +392,6 @@ function advancedSearch(post_type, filters, limit, existing_ids){
 		ajax_url,
 		user_data,
 		function(response){
-			console.log( response );
 			var json_posts = $.parseJSON(response);
 			var html_resultado;
 			var num_posts = -1;
