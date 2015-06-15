efe92160a52913a69140a081c381ce1eb8714b9c
zolitariuz <raul@zamacona.me>
2015-06-15 11:48:10 -0500

    FB pixel

--- themes/fotografica/header.php
+++ themes/fotografica/header.php
@@ -36,6 +36,23 @@
 		})(window,document,'script','dataLayer','GTM-MS3M6X');</script>
 		<!-- End Google Tag Manager -->
 
+		<script>(function() {
+		var _fbq = window._fbq || (window._fbq = []);
+		if (!_fbq.loaded) {
+		var fbds = document.createElement('script');
+		fbds.async = true;
+		fbds.src = '//connect.facebook.net/en_US/fbds.js';
+		var s = document.getElementsByTagName('script')[0];
+		s.parentNode.insertBefore(fbds, s);
+		_fbq.loaded = true;
+		}
+		_fbq.push(['addPixelId', '1133837239976877']);
+		})();
+		window._fbq = window._fbq || [];
+		window._fbq.push(['track', 'PixelInitialized', {}]);
+		</script>
+		<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?id=1133837239976877&amp;ev=PixelInitialized" /></noscript>
+
 		<script>
 			var fbApiInit = false;
 			var testId = '853048764736608';
