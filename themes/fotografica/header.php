e47593e807849a004dd8e5e3833ace8ca395e06f
zolitariuz <raul@zamacona.me>
2015-06-29 20:20:57 -0500

    intento 1 de menu

--- themes/fotografica/header.php
+++ themes/fotografica/header.php
@@ -189,7 +189,7 @@
 											</h1>
 										</div>
 										<div class="[ columna xmall-2 ][ text-right ]">
-											<a class="[ inline-block ][ menu__item menu__item-large ]" href="http://www.fundaciontelevisa.org/" target="_blank">
+											<a class="[ inline-block ][ menu__item menu__item-medium ]" href="http://www.fundaciontelevisa.org/" target="_blank">
 												<i class="[ icon-logo-fundacion-televisa ]"></i>
 											</a>
 										</div>
